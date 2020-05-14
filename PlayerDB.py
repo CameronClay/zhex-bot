e98ee29a3966b2441e6e871df32022db9354cb80
Cameron <cameronpclay@gmail.com>
2020-05-14 13:05:51 -0600

    Added date to db

--- PlayerDB.py
+++ PlayerDB.py
@@ -2,15 +2,16 @@ import sqlite3
 from Player import Player
 
 class PlayerDB:
-    DATABASE_NAME = 'players'
+    DATABASE_NAME = 'players.db'
     def __init__(self):
         self.conn = sqlite3.connect(PlayerDB.DATABASE_NAME)
         cur = self.conn.cursor()
         cur.execute('''create table if not exists players                  
-                    (id      text,
-                    wins     INTEGER, 
-                    loses    INTEGER,
-                    elo      REAL);''')
+                    (id        text     primary key,
+                    wins       INTEGER, 
+                    loses      INTEGER,
+                    elo        REAL,
+                    lastPlayed text);''')
 
     def IsRegistered(self, playerId):
         return self.Find(playerId) != None
@@ -20,8 +21,8 @@ class PlayerDB:
         return res.fetchone()
 
     def Register(self, player):
-        self.conn.execute('''insert into players (id, wins, loses, elo)
-                    values (?, ?, ?, ?)''', (player.id, player.wins, player.loses, player.elo))
+        self.conn.execute('''insert into players (id, wins, loses, elo, lastPlayed)
+                    values (?, ?, ?, ?, ?)''', (player.id, player.wins, player.loses, player.elo, player.lastPlayed))
         self.conn.commit()
 
     def UnRegister(self, playerId):
@@ -29,9 +30,9 @@ class PlayerDB:
         self.conn.commit()
 
     def UpdateStats(self, player):
-        self.conn.execute(f'''update players (id, wins, loses, elo) 
+        self.conn.execute(f'''update players (id, wins, loses, elo, lastPlayed) 
             set wins = {player.wins}, loses = {player.loses}, elo = {player.elo}
-            where id = {player.id}''')
+            where id = {player.id}, lastPlayed = {player.lastPlayed}''')
         self.conn.commit()
         
     def Close(self):
